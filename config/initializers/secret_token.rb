bba393b68cb86c1a8a772be66a82d72c84d315a8
Juliano Dasilva <julianodasilva@gmail.com>
2013-11-11 19:41:47 -0600

    create rails generate model notes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenCamp::Application.config.secret_key_base = 'fd0179aa050f9d2b5e245674b21071b85a14aed9ab1779bbf08dbcf78c9c8cbd6be68757851f47a9aca70a00f1f0aa5c50bc54d107c6e7ef07b2d01ab6d83ba2'
